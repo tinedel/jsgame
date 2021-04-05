9eeb8bd11061b9e31404e626b814201a1b308fee
tinedel <tinedel@tinedel.kiev.ua>
2021-04-05 21:14:14 +0200

    Update game.js

--- game.js
+++ game.js
@@ -3,10 +3,10 @@ let bottomplayer = document.querySelector('.bottomplayer');
 let ball = document.querySelector('.ball');
 
 let bottom = document.querySelector('.bottom');
-let top = document.querySelector('.top');
+let topc = document.querySelector('.top');
 let field = document.querySelector('.field');
 
-topplayer.style.left=((top.clientWidth - topplayer.clientWidth)/2) + "px";
+topplayer.style.left=((topc.clientWidth - topplayer.clientWidth)/2) + "px";
 bottomplayer.style.left=((bottom.clientWidth - bottomplayer.clientWidth)/2) + "px";
 ball.style.left=((field.clientWidth - ball.clientWidth)/2) + "px";
 ball.style.top=((field.clientHeight - ball.clientHeight)/2) + "px";
