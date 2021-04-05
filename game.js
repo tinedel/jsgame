17db2682a047714dd34ab484715d89505013fbab
tinedel <tinedel@tinedel.kiev.ua>
2021-04-05 20:59:24 +0200

    Create game.js

---
+++ game.js
@@ -0,0 +1,6 @@
+let topplayer = document.querySelector('topplayer');
+let bottomplayer = document.querySelector('bottomplayer');
+let ball = document.querySelector('ball');
+
+topplayer.left=500;
+bottomplayer.left=500;
